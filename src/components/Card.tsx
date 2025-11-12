93276bb1641ac3172b4cc40c4afd598e8d4a4660
Jofre Bedón <jjm.bedon@yavirac.edu.ec>
2025-11-12 14:52:55 -0500

    fracmentacion en card aprenderse

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,27 +1,70 @@
-import { CardContainer } from './CardContainer';
-import { CardBody } from './CardBody';
-import { CardTitle } from './CardTitle';
-import { CardText } from './CardText';
-import { CardLink } from './CardLink';
-import { CardFooter } from './CardFooter'; // Nueva importación
-
 function Card() {
+  function CardHeader() {
+    return (
+      <img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5h2z5FWVFGZFYnWldhNrEngvNMNdKHOGinA&s"
+        className="card-img-top"
+        alt="Imagen de ejemplo"
+      />
+    );
+  }
+
+  function CardBody() {
+    return (
+      <div className="card-body">
+        <h5 className="card-title text-info fw-bold">Card title</h5>
+        <p className="card-text">
+          Some quick example text to build on the card title and make up the
+          bulk of the card’s content.
+        </p>
+        <div className="d-flex justify-content-between">
+          <a href="#" className="btn btn-success">
+            Go somewhere
+          </a>
+          <button className="btn btn-outline-danger">
+            Details
+          </button>
+        </div>
+      </div>
+    );
+  }
+
+  function CardFooter() {
+    return (
+      <div className="card-footer text-muted text-center">
+        Updated 2 days ago
+      </div>
+    );
+  }
+
   return (
-    <CardContainer width="380px"> 
-
-
-      <CardBody>
-        <CardTitle title="Un Título Más Atractivo" />
-        <CardText text="Este es un ejemplo de texto más descriptivo para nuestra hermosa tarjeta. Podemos añadir más detalles aquí para hacerla interesante." />
-        
-        <CardLink href="#" label="Descubre Más" variant="dark" /> 
-      </CardBody>
-
-      <CardFooter>
-        <span>Última actualización: Hoy</span>
-      </CardFooter>
-    </CardContainer>
+    <div className="card shadow-lg border-0" style={{ width: "18rem" }}>
+      <CardHeader />
+      <CardBody />
+      <CardFooter />
+    </div>
   );
 }
 
-export default Card;
+function CardContainer() {
+  return (
+    <div className="container py-4">
+      <div className="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4">
+        <div className="col">
+          <Card />
+        </div>
+        <div className="col">
+          <Card />
+        </div>
+        <div className="col">
+          <Card />
+        </div>
+        <div className="col">
+          <Card />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CardContainer;
